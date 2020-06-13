d3e1df0801e84b87a4202ec66a0714992f7bf812
Ji Min Song <sjm9509@gmail.com>
2020-06-14 03:49:38 +0900

    update one more

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -4,22 +4,23 @@
 
 /* eslint-env jquery */
 
-$('document').ready(function(){
+$(document).ready(function(){
     
-    $('.js--section-features')waypoint(function(direction) {
+    $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky'); 
         }
-          
+    }, {
+        offset: '60px;'
     });
     
     
     /*
          var waypoints = $('#handler-first').waypoint(function(direction) {
           notify(this.element.id + ' hit 25% from top of window') 
-        }, {
+        }, { 
           offset: '25%'
         })
     */
